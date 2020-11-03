f572b3ec2ed7ef18068b51ef95624af226f0dd25
Niklas <nicki_bu@gmx.de>
2020-11-03 17:48:09 +0100

    Init Commit

---
+++ json/ImgDetails.js
@@ -0,0 +1,79 @@
+var data = "[" + 
+        '{"title": "An der Lahder Aue", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Feldrain", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Meßlinger Wäldchen", "year": "1983", "format": "106 x 82 cm", "technic": "Öl auf Spanplatte"},'+
+        '{"title": "Hamme bei Worpswede", "year": "2000", "format": "70 x 50 cm", "technic": "Öl auf Spanplatte"},'+
+        '{"title": "Weide an der Weser bei Petershagen", "year": "2020", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Gletschereis", "year": "2017", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Wegbiegung bei Morhoff I", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Lichtung", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Messlingen - Blick aufs Dorf", "year": "2007", "format": "24 x 18 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Nebelmorgen bei Wegholm", "year": "2011", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Überschwemmung an der Lahder Aue", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Schneesturm Grüntensee", "year": "20xx", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldweg", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldweg Mindener Wald", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldweg mit Hochsitzschatten I", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldweg mit Hochsitzschatten II", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Wales - The Arch", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Regenmorgen am Anleger", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Weserüberschwemmung an der  Auemündung", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Fishguard, Wales", "year": "2020", "format": "24 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Himmels- und Farbstudie", "year": "2020", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Langes Wald 1", "year": "2020", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Langes Wald 2", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Kiessee in Lahde", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Gletscher", "year": "2017", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Himmelsstudie mit durchbrechender Sonne", "year": "2004", "format": "100_x_80 cm", "technic": "Öl auf Spanplatte"},'+
+        '{"title": "Hünengrab an der Küste, Wales", "year": "2017", "format": "30 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Maschinenbild I", "year": "2001", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Maschinenbild Zara", "year": "1995", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Maschinenbild II", "year": "2001", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Himmelsstudie", "year": "2004", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Mein Schulweg", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Kiessee in Lahde", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Lahder Weserseite", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Schneestudie Wiehengebirge", "year": "2017", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Schuppen zwischen Heimsen und Döhren", "year": "2005", "format": "30 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Kraulschwimmer", "year": "1983", "format": "100 x 80 cm", "technic": "Öl auf Spanplatte"},'+
+        '{"title": "Handballer beim Sprungwurf", "year": "1984", "format": "120 x 100 cm", "technic": "Öl auf Spannplatte"},'+
+        '{"title": "Teufelsmoor mit Moorgraben", "year": "2016", "format": "90 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Überschwemmte Pappeln an der Auemündung", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Pentre Ifan II, Wales", "year": "2020", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Pentre Ifan I, Wales", "year": "2017", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Regentag - Wegbiegung bei Morhoff", "year": "2020", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Schlüsselburger Scheune I - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Öl auf Karton"},'+
+        '{"title": "Schlüsselburger Scheune II - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Öl auf Karton"},'+
+        '{"title": "Schober bei Wald, Allgäu", "year": "2017", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Todtenhauser Kirche im Schnee", "year": "2014", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Vereister Kiessee bei Frille", "year": "2003", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Messlinger Wäldchen mit Rapsfeld I", "year": "2000", "format": "80 x 60 cm", "technic": "Öl auf Spanplatte"},'+
+        '{"title": "Messlinger Wäldchen mit Rapsfeld II", "year": "2000", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldrandstudie Nordholz", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldstudie mit Abendsonne", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldrandstudie", "year": "2020", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Waldstudie Baumstämme", "year": "2019", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Weserblick von Heisterholz nach Süden", "year": "2019", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Weser bei Holzminden", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Weserüberschwemmung bei Petershagen", "year": "2009", "format": "32 x 24 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Am Nordholz II", "year": "2020", "format": "80 x 60 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Weser bei Regen Lahder Ufer", "year": "2020", "format": "40 x 30 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Windheimer Kirche", "year": "2000", "format": "24 x 18 cm", "technic": "Öl auf Leinwand"},'+
+        '{"title": "Wolkenstudie", "year": "2018", "format": "24 x 18 cm", "technic": "Öl auf Leinwand"},'+
+
+        '{"title": "Die Sonne kommt durch bei Emden", "year": "2001", "format": "40 x 30 cm", "technic": "Aquarell"},'+
+	'{"title": "Die Petrikirche in Petershagen", "year": "2002", "format": "30 x 40 cm", "technic": "Aquarell"},'+
+	'{"title": "Die St.-Johannes-Kirche in Petershagen", "year": "2002", "format": "40 x 30 cm", "technic": "Aquarell"},'+
+	'{"title": "Nebelmorgen bei Südfelde", "year": "2001", "format": "24 x 18 cm", "technic": "Aquarell"},'+
+	
+	'{"title": "Der Bodelsberg 1", "year": "2008", "format": "32 x 24 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},'+
+        '{"title": "Der Bodelsberg 2", "year": "2008", "format": "32 x 24 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},'+
+        '{"title": "Martins Nagelfluh, Bodelsberg", "year": "2008", "format": "24 x 18 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},'+
+        '{"title": "Nagelfluh, Bodelsberg", "year": "2008", "format": "24 x 18 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},'+
+        '{"title": "Donaudurchbruch 1", "year": "2009", "format": "32 x 24 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},'+
+        '{"title": "Donaudurchbruch 2", "year": "2009", "format": "24 x 32 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},'+
+        '{"title": "Bei Obernbauernschaft", "year": "2001", "format": "24 x 18 cm", "technic": "Zeichnung Tusche und Pastell auf Packpapier"}'+
+
+        
+        "]"
+        
