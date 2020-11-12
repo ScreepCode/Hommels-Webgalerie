f16a0a71521741f85e50ea73cbff5898bf9d8735
Niklas <nicki_bu@gmx.de>
2020-11-12 19:21:56 +0100

    Fixed: Few Pictures

--- json/ImgDetails.js
+++ json/ImgDetails.js
@@ -10,7 +10,7 @@ var data = "[" +
         '{"title": "Lichtung", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Messlingen - Blick aufs Dorf", "year": "2007", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Nebelmorgen bei Wegholm", "year": "2011", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Überschwemmung an der Lahder Aue", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Ueberschwemmung an der Lahder Aue", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Schneesturm Gruentensee, Allgaeu", "year": "2016", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Waldweg", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Waldweg Mindener Wald", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
@@ -35,17 +35,17 @@ var data = "[" +
         '{"title": "Kiessee in Lahde", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Lahder Weserseite", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Schneestudie Wiehengebirge", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Schuppen zwischen Heimsen und Döhren", "year": "2005", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Schuppen zwischen Heimsen und Doehren", "year": "2005", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Kraulschwimmer", "year": "1983", "format": "100 x 80 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Handballer beim Sprungwurf", "year": "1984", "format": "120 x 100 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Teufelsmoor mit Moorgraben", "year": "2016", "format": "90 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Überschwemmte Pappeln an der Auemündung", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Ueberschwemmte Pappeln an der Auemuendung", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Pentre Ifan II, Wales", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Pentre Ifan I, Wales", "year": "2017", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Regentag - Wegbiegung bei Morhoff", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Schlüsselburger Scheune I - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Oel auf Karton"},' + 
-        '{"title": "Schlüsselburger Scheune II - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Oel auf Karton"},' + 
-        '{"title": "Schober bei Wald, Allgäu", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Schluesselburger Scheune I - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Oel auf Karton"},' + 
+        '{"title": "Schluesselburger Scheune II - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Oel auf Karton"},' + 
+        '{"title": "Schober bei Wald, Allgaeu", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Todtenhauser Kirche im Schnee", "year": "2014", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Vereister Kiessee bei Frille", "year": "2003", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Meszlinger Waeldchen mit Rapsfeld I", "year": "2000", "format": "80 x 60 cm", "technic": "Oel auf Spanplatte"},' + 
@@ -53,18 +53,18 @@ var data = "[" +
         '{"title": "Waldrandstudie Nordholz", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Waldstudie mit Abendsonne", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Waldrandstudie", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Waldstudie Baumstämme", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Weserblick von Heisterholz nach Süden", "year": "2019", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Waldstudie Baumstaemme", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Weserblick von Heisterholz nach Sueden", "year": "2019", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Weser bei Holzminden", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Weserüberschwemmung bei Petershagen", "year": "2009", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Weserueberschwemmung bei Petershagen", "year": "2009", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Am Nordholz II", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Weser bei Regen Lahder Ufer", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Weser bei Regen, Lahder Ufer", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Windheimer Kirche", "year": "2000", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Wolkenstudie", "year": "2018", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Die Sonne kommt durch bei Emden", "year": "2001", "format": "40 x 30 cm", "technic": "Aquarell"},' + 
         '{"title": "Die Petrikirche in Petershagen", "year": "2002", "format": "30 x 40 cm", "technic": "Aquarell"},' + 
         '{"title": "Die St.-Johannes-Kirche in Petershagen", "year": "2002", "format": "40 x 30 cm", "technic": "Aquarell"},' + 
-        '{"title": "Nebelmorgen bei Südfelde", "year": "2001", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
+        '{"title": "Nebelmorgen bei Suedfelde", "year": "2001", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
         '{"title": "Der Bodelsberg 1", "year": "2008", "format": "32 x 24 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},' + 
         '{"title": "Der Bodelsberg 2", "year": "2008", "format": "32 x 24 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},' + 
         '{"title": "Martins Nagelfluh, Bodelsberg", "year": "2008", "format": "24 x 18 cm", "technic": "Zeichnung Pastellzeichnung auf Ingrespapier"},' + 
@@ -102,18 +102,18 @@ var data = "[" +
         '{"title": "Schloss Petershagen", "year": "1985", "format": "30 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Meszlingen Dorfstraszenecke II", "year": "1985", "format": "32 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Selbstbildnis", "year": "1968", "format": "24 x 32 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
-        '{"title": "Blick auf Eldagsen an Pfingsten", "year": "1982", "format": "30 x 24 cm", "technic": "Aquarell"},' + 
-        '{"title": "El Qusir - Wasserverkauf an der Strasze", "year": "2003", "format": "30 x 40 cm", "technic": "Zeichnung"},' + 
+        '{"title": "Blick auf Eldagsen an Pfingsten 82", "year": "1982", "format": "30 x 24 cm", "technic": "Aquarell"},' + 
+        '{"title": "El Qusir - Wasserverkauf an der Strasze", "year": "2003", "format": "30 x 40 cm", "technic": "Zeichnung Pastell und Feder"},' + 
         '{"title": "Erster Hof in Ostwestfalen", "year": "1979", "format": "20 x 15 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
         '{"title": "Gelbe Saecke", "year": "2004", "format": "80 x 60 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Kerzenhalter und Apfel, Studie", "year": "2012", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
         '{"title": "Lahder Weserseite", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Mauritius I, Unwetter", "year": "2002", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Mauridrohender Regentius II, ", "year": "2002", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Mauridrohender Regentius II", "year": "2002", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Meszlinger Waeldchen mit Rapsfeld III", "year": "2004", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Salisbury Cathedral", "year": "2020", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Salisbury Cathedral", "year": "2020", "format": "32 x 24 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Waldstudie Baumstaemme", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Walisische Kueste", "year": "2017", "format": "24  cmx 18", "technic": "Aquarell"},' + 
+        '{"title": "Walisische Kueste", "year": "2017", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
         '{"title": "Weser bei Regen, Lahder Ufer", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Zukunft", "year": "1982", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"}' + 
         "]"
