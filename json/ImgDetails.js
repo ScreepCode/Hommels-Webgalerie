69e1bc39426754281d40f0aab87c9ce5b3fb5a52
Niklas <nicki_bu@gmx.de>
2020-11-19 17:39:50 +0100

    Update: JSON with new Pictures

--- json/ImgDetails.js
+++ json/ImgDetails.js
@@ -2,7 +2,6 @@
 var data = "[" + 
         '{"title": "An der Lahder Aue", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Feldrain", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Meszlinger Waeldchen", "year": "1983", "format": "106 x 82 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Hamme bei Worpswede", "year": "2000", "format": "70 x 50 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Weide an der Weser bei Petershagen", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Gletschereis", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
@@ -24,7 +23,7 @@ var data = "[" +
         '{"title": "Langes Wald 1", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Langes Wald 2", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Kiessee in Lahde", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Gletscher", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Riehebach ueberschwemmt", "year": "2003", "format": "47 x 56 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Himmelsstudie mit durchbrechender Sonne", "year": "2004", "format": "100_x_80 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Huenengrab an der Kueste, Wales", "year": "2017", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Maschinenbild I", "year": "2001", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
@@ -32,12 +31,10 @@ var data = "[" +
         '{"title": "Maschinenbild II", "year": "2001", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Himmelsstudie", "year": "2004", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Mein Schulweg", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Kiessee in Lahde", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Lahder Weserseite", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Schneestudie Wiehengebirge", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Schuppen zwischen Heimsen und Doehren", "year": "2005", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Kraulschwimmer", "year": "1983", "format": "100 x 80 cm", "technic": "Oel auf Spanplatte"},' + 
-        '{"title": "Handballer beim Sprungwurf", "year": "1984", "format": "120 x 100 cm", "technic": "Oel auf Spanplatte"},' + 
+        '{"title": "Schuppen zwischen Heimsen und Döhren", "year": "2005", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Kraulschwimmer", "year": "1983", "format": "105 x 74 cm", "technic": "Oel auf Spanplatte"},' + 
+        '{"title": "Handballer beim Sprungwurf", "year": "1984", "format": "80 x 100 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Teufelsmoor mit Moorgraben", "year": "2016", "format": "90 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Ueberschwemmte Pappeln an der Auemuendung", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Pentre Ifan II, Wales", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
@@ -45,7 +42,7 @@ var data = "[" +
         '{"title": "Regentag - Wegbiegung bei Morhoff", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Schluesselburger Scheune I - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Oel auf Karton"},' + 
         '{"title": "Schluesselburger Scheune II - Detail", "year": "2012", "format": "24 x 18 cm", "technic": "Oel auf Karton"},' + 
-        '{"title": "Schober bei Wald, Allgaeu", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Schober bei Wald, Allgäu", "year": "2017", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Todtenhauser Kirche im Schnee", "year": "2014", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Vereister Kiessee bei Frille", "year": "2003", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Meszlinger Waeldchen mit Rapsfeld I", "year": "2000", "format": "80 x 60 cm", "technic": "Oel auf Spanplatte"},' + 
@@ -58,7 +55,7 @@ var data = "[" +
         '{"title": "Weser bei Holzminden", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Weserueberschwemmung bei Petershagen", "year": "2009", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Am Nordholz II", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Weser bei Regen, Lahder Ufer", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Maison prés de Carnac, Bretagne", "year": "1997", "format": "120 x 100 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Windheimer Kirche", "year": "2000", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Wolkenstudie", "year": "2018", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Die Sonne kommt durch bei Emden", "year": "2001", "format": "40 x 30 cm", "technic": "Aquarell"},' + 
@@ -89,31 +86,96 @@ var data = "[" +
         '{"title": "Gasse III in El Qusir Vorstudie", "year": "2002", "format": "30 x 40 cm", "technic": "Aquarell"},' + 
         '{"title": "Kopfweiden am Riehebach", "year": "2003", "format": "24 x 32 cm", "technic": "Aquarell"},' + 
         '{"title": "Kriegsrequiem aus dem Fruehwerk", "year": "1968", "format": "24 x 30 cm", "technic": "Zeichnung Federzeichnung"},' + 
-        '{"title": "Selbstbildnis", "year": "1967", "format": "24 x 32 cm", "technic": "Zeichnung Kohlezeichnung"},' + 
+        '{"title": "Selbstbildnis I", "year": "1967", "format": "24 x 32 cm", "technic": "Zeichnung Kohlezeichnung"},' + 
         '{"title": "Wanderer (aus dem Fruehwerk)", "year": "1968", "format": "24 x 30 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Winter (aus dem Fruehwerk)", "year": "1968", "format": "24 x 30 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Dorf mit Burg", "year": "1980", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
         '{"title": "Haus des Gastes in Porta Westfalica", "year": "1985", "format": "40 x 30 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Winterhof", "year": "1991", "format": "30 x 24 cm", "technic": "Zeichnung Kohlezeichnung"},' + 
-        '{"title": "Meszlingen Dorfstraszenecke", "year": "1985", "format": "32 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
-        '{"title": "Haus eines Spieszbuergers", "year": "1985", "format": "30 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Meszlingen Dorfstraszenecke", "year": "1985", "format": "32 x 24 cm", "technic": "Zeichnung Federzeichnung laviert"},' + 
+        '{"title": "Haus eines Spieszbuergers", "year": "1985", "format": "30 x 24 cm", "technic": "Zeichnung_Federzeichnung"},' + 
         '{"title": "Im Dorf", "year": "1985", "format": "30 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Meszlinger Muehle", "year": "1987", "format": "30 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Schloss Petershagen", "year": "1985", "format": "30 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
         '{"title": "Meszlingen Dorfstraszenecke II", "year": "1985", "format": "32 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
-        '{"title": "Selbstbildnis", "year": "1968", "format": "24 x 32 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
-        '{"title": "Blick auf Eldagsen an Pfingsten 82", "year": "1982", "format": "30 x 24 cm", "technic": "Aquarell"},' + 
-        '{"title": "El Qusir - Wasserverkauf an der Strasze", "year": "2003", "format": "30 x 40 cm", "technic": "Zeichnung Pastell und Feder"},' + 
+        '{"title": "Selbstbildnis II", "year": "1968", "format": "24 x 32 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Blick auf Eldagsen an Pfingsten", "year": "1982", "format": "30 x 24 cm", "technic": "Aquarell"},' + 
+        '{"title": "El Qusir - Wasserverkauf an der Strasze", "year": "2003", "format": "30 x 40 cm", "technic": "Zeichnung Federzeichnung mit Pastell gehöht"},' + 
         '{"title": "Erster Hof in Ostwestfalen", "year": "1979", "format": "20 x 15 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
         '{"title": "Gelbe Saecke", "year": "2004", "format": "80 x 60 cm", "technic": "Oel auf Spanplatte"},' + 
-        '{"title": "Kerzenhalter und Apfel, Studie", "year": "2012", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
-        '{"title": "Lahder Weserseite", "year": "2020", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Kerzenhalter und Apfel Studie", "year": "2012", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
+        '{"title": "Kopfweiden in der Wesermarsch", "year": "1998", "format": "54 x 43 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Mauritius I, Unwetter", "year": "2002", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Mauridrohender Regentius II", "year": "2002", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Mauritius II, drohender Regen", "year": "2002", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Meszlinger Waeldchen mit Rapsfeld III", "year": "2004", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Salisbury Cathedral", "year": "2020", "format": "32 x 24 cm", "technic": "Oel auf Spanplatte"},' + 
         '{"title": "Waldstudie Baumstaemme", "year": "2019", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
         '{"title": "Walisische Kueste", "year": "2017", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
         '{"title": "Weser bei Regen, Lahder Ufer", "year": "2020", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"},' + 
-        '{"title": "Zukunft", "year": "1982", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"}' + 
+        '{"title": "Zukunft", "year": "1982", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Wiehengebirgsstudie mit Siedlung", "year": "2017", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Feld am Wiehengebirge", "year": "1988", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Kleines Maschinenbild", "year": "2003", "format": "65 x 55 cm", "technic": "Oel auf Spanplatte"},' + 
+        '{"title": "Maschinenbild IV", "year": "2001", "format": "70 x 50 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Maschinenbild V", "year": "2001", "format": "70 x 50 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Sturm Waeldchen Meszlingen IV", "year": "1998", "format": "24 x 18 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Synagoge in Petershagen vor der Sanierung", "year": "1990", "format": "120 x 100 cm", "technic": "Oel auf Spanplatte"},' + 
+        '{"title": "Todtenhauser Christuskirche", "year": "2003", "format": "32 x 24 cm", "technic": "Aquarell"},' + 
+        '{"title": "Selbstbildnis unvollendet", "year": "1979", "format": "70 x 90 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Hiroshima 8 Uhr 16", "year": "1995", "format": "40 x 50 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Schuh von Marlies", "year": "1979", "format": "24 x 30 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Alltagsstudie", "year": "1968", "format": "18 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Augenblicke", "year": "1966", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Bodelsberg Landschaftsstudie", "year": "1979", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Uchter Moor", "year": "1979", "format": "18 x 24 cm", "technic": "Aquarell"},' + 
+        '{"title": "Bibliophil", "year": "1983", "format": "18 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Stauwehr in Schluesselburg", "year": "2004", "format": "18 x 24 cm", "technic": "Zeichnung Buntstiftzeichnung"},' + 
+        '{"title": "Spieszbuerger II", "year": "1985", "format": "18 x 24 cm", "technic": "Zeichnung Federzeichnung Sepia"},' + 
+        '{"title": "Selbstbildnis mit 17 Jahren", "year": "1969", "format": "18 x 24 cm", "technic": "Zeichnung Filzstiftzeichnung"},' + 
+        '{"title": "Selbstbildnis als Student", "year": "1972", "format": "18 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Selbstbildnis als Dandy", "year": "1969", "format": "18 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Selbstbildnis in Bleistift", "year": "1966", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Rueckenakt", "year": "1966", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Nordhemmern", "year": "1979", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Mindener Rathaus", "year": "2001", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Mindener Dom I", "year": "1990", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Meszlingen Dorf spektral zerlegt", "year": "1998", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Mein Klavier in Meszlingen", "year": "1982", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Marlies Portraet", "year": "1973", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Linke Hand von unten", "year": "1968", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Katja uebt Physik", "year": "2002", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Handstudien", "year": "1968", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Guck mich nicht so an", "year": "1968", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Faltenstudie mit Maus", "year": "1975", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Die Presse Karikatur", "year": "1969", "format": "18 x 24 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Bodelsberg Studie mit Heustadeln", "year": "2007", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Gehlemuendung", "year": "1998", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
+        '{"title": "Hofskizze", "year": "1985", "format": "24 x 18 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Ingwerknolle", "year": "2001", "format": "24 x 18 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Kirche Schluesselburg", "year": "2020", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
+        '{"title": "Kirche Heimsen", "year": "2020", "format": "24 x 18 cm", "technic": "Aquarell"},' + 
+        '{"title": "Physik fuer Geisteswissenschaftler - das Photon", "year": "2002", "format": "24 x 18 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Rein koerperlich", "year": "1966", "format": "24 x 18 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Rueckenstudie", "year": "1966", "format": "24 x 18 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Schweinestudie", "year": "1979", "format": "24 x 18 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Walisische Kueste III", "year": "2020", "format": "24 x 18 cm", "technic": "Zeichnung Federzeichnung aquarelliert"},' + 
+        '{"title": "Wer Ohren hat", "year": "1966", "format": "24 x 18 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Wurzeln", "year": "2001", "format": "24 x 18 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Baumstudie IV", "year": "1999", "format": "24 x 30 cm", "technic": "Aquarell"},' + 
+        '{"title": "Kopfweiden in Meiszen", "year": "1981", "format": "24 x 30 cm", "technic": "Zeichnung Kohlezeichnung"},' + 
+        '{"title": "Mine is better than the common splatter Tryptichon", "year": "2015", "format": "24 x 54 cm", "technic": "Oel auf Spanplatte"},' + 
+        '{"title": "Loewengesicht", "year": "1966", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Eichhoernchen", "year": "1966", "format": "18 x 24 cm", "technic": "Zeichnung Bleistiftzeichnung"},' + 
+        '{"title": "Petershaeger Strasze", "year": "1984", "format": "24 x 18 cm", "technic": "Zeichnung Federzeichnung"},' + 
+        '{"title": "Farborgie", "year": "1973", "format": "60 x 80 cm", "technic": "Oel auf Karton"},' + 
+        '{"title": "Meszlinger Waeldchen ohne Allee III", "year": "1998", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Eldagsen", "year": "West", "format": "2007 40 x 30", "technic": "cm Oel auf Leinwand"},' + 
+        '{"title": "Meszlinger Waeldchen ohne Allee II", "year": "2002", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Meszlinger Waeldchen ohne Allee I", "year": "1983", "format": "106 x 84 cm", "technic": "Oel auf Spanplatte"},' + 
+        '{"title": "Waeldchen bei Suedfelde im Schnee", "year": "2001", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Ueberschwemmung Riehebach II", "year": "2004", "format": "48 x 56 cm", "technic": "Oel auf Spanplatte im Zierrahmen"},' + 
+        '{"title": "Eldagsen West", "year": "2007", "format": "40 x 30 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Bodelsberg Stadel I", "year": "2007", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' + 
+        '{"title": "Wassermuehle in Doehren", "year": "2002", "format": "30 x 24 cm", "technic": "Aquarell"},' + 
+        '{"title": "Wales; Bucht bei St Annes Head", "year": "2018", "format": "80 x 60 cm", "technic": "Oel auf Leinwand"}' + 
         "]"
