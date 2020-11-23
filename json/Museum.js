0459a66bc76829f77c2368ca3385ff1ded7783ab
Niklas <nicki_bu@gmx.de>
2020-11-23 21:49:14 +0100

    Add: MuseumsBilder

--- json/Museum.js
+++ json/Museum.js
@@ -1,3 +1,10 @@
 var data = "[" + 
-        
+        '{"title": "Alhambra", "year": "2019", "format": "100 x 80 cm", "technic": "Oel auf Leinwand"},' +
+        '{"title": "Blick ueber die Weser von Doehren nach Windheim", "year": "2004", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' +
+        // '{"title": "Es wird nie wieder so", "year": "2020", "format": "80 x 60 cm", "technic": ""},' +
+        '{"title": "Kopfweiden in der Joesser Aue", "year": "2003", "format": "30 x 24 cm", "technic": "Oel auf Leinwand"},' +
+        '{"title": "Korallenblock im Roten Meer", "year": "2018", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"},' +
+        // '{"title": "Korallenriff an der Giftuninsel", "year": "2004", "format": "24 x 18 cm", "technic": ""},' +
+        '{"title": "Walisische Kueste bei Newgale", "year": "2019", "format": "24 x 18 cm", "technic": "Aquarell"},' +
+        '{"title": "Zwischen Heimsen und Doehren", "year": "2003", "format": "32 x 24 cm", "technic": "Oel auf Leinwand"}' +
         "]"
